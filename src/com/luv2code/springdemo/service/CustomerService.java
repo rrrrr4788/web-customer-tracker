a348132ea882da5a024e9e6ca57595b69f986203
Haozhe Zhang <zhz19980514@gmail.com>
2022-07-24 00:18:10 -0400

    feat: customer crud

--- src/com/luv2code/springdemo/service/CustomerService.java
+++ src/com/luv2code/springdemo/service/CustomerService.java
@@ -1,14 +1,14 @@
-package com.luv2code.springdemo.dao;
+package com.luv2code.springdemo.service;
 
 import com.luv2code.springdemo.entity.Customer;
 
 import java.util.List;
 
-public interface CustomerDAO {
+public interface CustomerService {
 
     public List<Customer> getCustomers();
 
-    void saveCustomer(Customer theCustomer);
+    public void saveCustomer(Customer theCustomer);
 
     Customer getCustomer(int theId);
 
